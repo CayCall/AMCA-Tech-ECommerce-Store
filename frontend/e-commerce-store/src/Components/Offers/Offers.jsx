e1d18643551f92d8c73b3eda2a5a7c0a2e6aacba
Caleb Cameron Calvin <calvincaleb09@gmail.com>
2025-05-18 21:31:58 +0200

    started exclusive section

---
+++ frontend/e-commerce-store/src/Components/Offers/Offers.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Offers.css'
+import phoneImage from '../Assets/i12_4.png'
+
+const Offers = () => {
+    return (
+        <div className='offers'>
+            <div className='offers-left'>
+                <h1 className='offers-h1-first'>Exclusive Offers For You</h1>
+                <h1 className='offers-h1-second'>GET TECH TODAY</h1>
+                <button className='btn-variation-1'>Learn More</button>
+            </div>
+        </div>
+    )
+}
+
+export default Offers
--- frontend/e-commerce-store/src/Pages/Home.jsx
+++ frontend/e-commerce-store/src/Pages/Home.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Shop from './Shop'
 import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
 const Home = () => {
   return (
     <div>
       <Hero/>
       <Popular/>
-
-
+      <Offers/>
     </div>
   )
 }
